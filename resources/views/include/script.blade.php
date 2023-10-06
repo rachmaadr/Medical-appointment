2006b3c75763a7a84aa23a4380a18c430dd09320
Rachmad Dhani <email@example.com>
2023-10-07 01:00:31 +0700

    first commit

---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,38 @@
+<!-- jquery Min JS -->
+<script src="{{ asset("asset/js/jquery.min.js") }}"></script>
+<!-- jquery Migrate JS -->
+<script src="{{ asset("asset/js/jquery-migrate-3.0.0.js") }}"></script>
+<!-- jquery Ui JS -->
+<script src="{{ asset("asset/js/jquery-ui.min.js") }}"></script>
+<!-- Easing JS -->
+<script src="{{ asset("asset/js/easing.js") }}"></script>
+<!-- Color JS -->
+<script src="{{ asset("asset/js/colors.js") }}"></script>
+<!-- Popper JS -->
+<script src="{{ asset("asset/js/popper.min.js") }}"></script>
+<!-- Bootstrap Datepicker JS -->
+<script src="{{ asset("asset/js/bootstrap-datepicker.js") }}"></script>
+<!-- Jquery Nav JS -->
+<script src="{{ asset("asset/js/jquery.nav.js") }}"></script>
+<!-- Slicknav JS -->
+<script src="{{ asset("asset/js/slicknav.min.js") }}"></script>
+<!-- ScrollUp JS -->
+<script src="{{ asset("asset/js/jquery.scrollUp.min.js") }}"></script>
+<!-- Niceselect JS -->
+<script src="{{ asset("asset/js/niceselect.js") }}"></script>
+<!-- Tilt Jquery JS -->
+<script src="{{ asset("asset/js/tilt.jquery.min.js") }}"></script>
+<!-- Owl Carousel JS -->
+<script src="{{ asset("asset/js/owl-carousel.js") }}"></script>
+<!-- counterup JS -->
+<script src="{{ asset("asset/js/jquery.counterup.min.js") }}"></script>
+<!-- Steller JS -->
+<script src="{{ asset("asset/js/steller.js") }}"></script>
+<!-- Wow JS -->
+<script src="{{ asset("asset/js/wow.min.js") }}"></script>
+<!-- Magnific Popup JS -->
+<script src="{{ asset("asset/js/jquery.magnific-popup.min.js") }}"></script>
+<!-- Bootstrap JS -->
+<script src="{{ asset("asset/js/bootstrap.min.js") }}"></script>
+<!-- Main JS -->
+<script src="{{ asset("asset/js/main.js") }}"></script>
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    {{-- Google Font, CSS, Bootstrap --}}
+    @include("include.style")
+
+</head>
+<body>
+    {{-- Header --}}
+    @include("include.header")
+
+    {{-- Content --}}
+    @include("page")
+
+    {{-- Footer --}}
+    @include("include.footer")
+
+    {{-- JS Resource --}}
+    @include("include.script")
+</body>
+</html>
